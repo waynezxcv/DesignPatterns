ca0b22bc99fb6776e44738cc926613a01b3aef01
waynezxcv <liuweiself@126.com>
2017-07-09 17:12:44 +0800

    First commit

---
+++ DesignPatterns/DesignPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPatterns
+//
+//  Created by 刘微 on 2017/7/9.
+//  Copyright © 2017年 waynezxcv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPatterns/DesignPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatterns
+//
+//  Created by 刘微 on 2017/7/9.
+//  Copyright © 2017年 waynezxcv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
